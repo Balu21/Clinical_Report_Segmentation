a96c1afb9a38fe6674aac38410310485dc94eba9
Balavenkat Gottimukkala <43732471+Balu21@users.noreply.github.com>
2020-01-19 01:10:54 -0700

    Add files via upload

---
+++ Clincal_segmentation.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+tqdm.pandas(desc="progress-bar")
+from gensim.models import Doc2Vec
+from sklearn import utils
+from sklearn.model_selection import train_test_split
+import gensim
+from sklearn.linear_model import LogisticRegression
+from gensim.models.doc2vec import TaggedDocument
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data_cleaned.tsv', sep='\t',error_bad_lines=False)
+
+df.head()
+
+df['SECTION_TEXT'][1]
+
+df.shape
+
+df = df.dropna()
+df.shape
+
+df['HEADER_TYPE'].value_counts()
+
+
+
+df.head()
+
+from gensim.models import doc2vec
+def label_sentences(corpus, label_type):
+    """
+    Gensim's Doc2Vec implementation requires each document/paragraph to have a label associated with it.
+    We do this by using the TaggedDocument method. The format will be "TRAIN_i" or "TEST_i" where "i" is
+    a dummy index of the clinical notes.
+    """
+    labeled = []
+    for i, v in enumerate(corpus):
+        label = label_type + '_' + str(i)
+        labeled.append(doc2vec.TaggedDocument(v.split(), [label]))
+    return labeled
+
+
+X_train, X_test, y_train, y_test = train_test_split(df.SECTION_TEXT, df.HEADER_TYPE, random_state=0, test_size=0.3)
+X_train = label_sentences(X_train, 'Train')
+X_test = label_sentences(X_test, 'Test')
+all_data = X_train + X_test
+
+len(all_data)
+
+all_data[:1]
+
+model_dbow = Doc2Vec(dm=0, vector_size=300, negative=5, min_count=1, alpha=0.065, min_alpha=0.065)
+model_dbow.build_vocab([x for x in tqdm(all_data)])
+
+%%time
+for epoch in range(30):
+    model_dbow.train(utils.shuffle([x for x in tqdm(all_data)]), total_examples=len(all_data), epochs=1)
+    model_dbow.alpha -= 0.002
+    model_dbow.min_alpha = model_dbow.alpha
+
+def get_vectors(model, corpus_size, vectors_size, vectors_type):
+    """
+    Get vectors from trained doc2vec model
+    :param doc2vec_model: Trained Doc2Vec model
+    :param corpus_size: Size of the data
+    :param vectors_size: Size of the embedding vectors
+    :param vectors_type: Training or Testing vectors
+    :return: list of vectors
+    """
+    vectors = np.zeros((corpus_size, vectors_size))
+    for i in range(0, corpus_size):
+        prefix = vectors_type + '_' + str(i)
+        vectors[i] = model.docvecs[prefix]
+    return vectors
+
+train_vectors_dbow = get_vectors(model_dbow, len(X_train), 300, 'Train')
+test_vectors_dbow = get_vectors(model_dbow, len(X_test), 300, 'Test')
+
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression(multi_class='multinomial', solver = 'lbfgs')
+logreg.fit(train_vectors_dbow, y_train)
+
+logreg.score(test_vectors_dbow, y_test)
+
+%%time
+
+from sklearn.naive_bayes import GaussianNB
+
+model = GaussianNB()
+model.fit(train_vectors_dbow, y_train)
+model.score(test_vectors_dbow, y_test)
